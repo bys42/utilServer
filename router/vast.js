e4e62227248862d5add964a89ece4e62004003b3
ben.su <pumapupa@gmail.com>
2025-01-10 14:37:24 +0800

    chore: coding style

--- router/vast.js
+++ router/vast.js
@@ -1,39 +1,38 @@
-const Router = require('koa-router')
-const fs = require('fs').promises;
-const path = require('path');
+const Router = require("koa-router");
+const fs = require("fs").promises;
+const path = require("path");
 
 module.exports = {
     getRouter: () => {
-        const router = new Router()
-        router.prefix('/vast')
+        const router = new Router();
+        router.prefix("/vast");
 
-        router.get('/:id', async (ctx) => {
-                try {
-                    const dir = ctx.secure ? "vast_https" : "vast_http";
-                    const { id } = ctx.params;
-                    const filePath = path.join(__dirname, `../static/${dir}/${id}.xml`);
-                    const xmlContent = await fs.readFile(filePath, 'utf-8');
-                    ctx.type = 'application/xml';
-                    ctx.body = xmlContent;
+        router.get("/:id", async (ctx) => {
+            try {
+                const dir = ctx.secure ? "vast_https" : "vast_http";
+                const { id } = ctx.params;
+                const filePath = path.join(__dirname, `../static/${dir}/${id}.xml`);
+                const xmlContent = await fs.readFile(filePath, "utf-8");
+                ctx.type = "application/xml";
+                ctx.body = xmlContent;
+            } catch (error) {
+                console.error("讀取文件錯誤:", error);
 
-                } catch (error) {
-                console.error('讀取文件錯誤:', error);
-
-                if (error.code === 'ENOENT') {
-                        ctx.status = 404;
-                        ctx.body = {
-                        error: '找不到指定的 VAST 文件'
-                        };
-                        return;
-                    }
-
-                    ctx.status = 500;
+                if (error.code === "ENOENT") {
+                    ctx.status = 404;
                     ctx.body = {
-                    error: '服務器內部錯誤'
+                        error: "找不到指定的 VAST 文件",
                     };
+                    return;
                 }
-        })
 
-        return router.routes()
-    }
-}
+                ctx.status = 500;
+                ctx.body = {
+                    error: "服務器內部錯誤",
+                };
+            }
+        });
+
+        return router.routes();
+    },
+};
